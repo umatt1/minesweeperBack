d8d482b1fe17a5c9c4fb3d9bd641cb67ca231014
umatt1 <matt.burkhard@gmail.com>
2023-10-11 21:45:50 -0500

    Fixes imports in repository

--- minesweeper/src/main/java/com/minesweeper/repo/SolveRepository.java
+++ minesweeper/src/main/java/com/minesweeper/repo/SolveRepository.java
@@ -1,6 +1,7 @@
 package com.minesweeper.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.minesweeper.model.Solve;
 
 public interface SolveRepository extends JpaRepository<Solve, Long> {
     // You can add custom query methods here if needed
--- minesweeper/src/main/java/com/minesweeper/repo/UserRepository.java
+++ minesweeper/src/main/java/com/minesweeper/repo/UserRepository.java
@@ -1,7 +1,7 @@
 package com.minesweeper.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.minesweeper.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     // You can add custom query methods here if needed
 }
