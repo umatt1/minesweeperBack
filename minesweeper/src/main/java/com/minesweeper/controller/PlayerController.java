c48b4cbac3d56a870d195e0de26842cea6bd7f77
umatt1 <matt.burkhard@gmail.com>
2023-11-18 19:22:50 -0500

    Starts to try and add mappings that can be used

--- minesweeper/src/main/java/com/minesweeper/controller/PlayerController.java
+++ minesweeper/src/main/java/com/minesweeper/controller/PlayerController.java
@@ -1,17 +1,39 @@
 package com.minesweeper.controller;
 
-
+import com.minesweeper.model.Player;
 import com.minesweeper.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-// PlayerController.java
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
+
     @Autowired
     private PlayerService playerService;
 
-    // CRUD operations for Player entity
+    @GetMapping
+    public List<Player> getAllPlayers() {
+        return playerService.getAllPlayers();
+    }
+
+    @GetMapping("/{playerId}")
+    public Optional<Player> getPlayerById(@PathVariable Long playerId) {
+        return playerService.getPlayerById(playerId);
+    }
+
+    @PostMapping
+    public Player createPlayer(@RequestBody Player player) {
+        return playerService.createPlayer(player);
+    }
+
+    @DeleteMapping("/{playerId}")
+    public void deletePlayer(@PathVariable Long playerId) {
+        playerService.deletePlayer(playerId);
+    }
+
+    // You can add additional methods as needed
 }
--- minesweeper/src/main/java/com/minesweeper/model/Player.java
+++ minesweeper/src/main/java/com/minesweeper/model/Player.java
@@ -16,4 +16,29 @@ public class Player {
     private String password;
 
     // Constructors, getters, setters, and other methods
+
+    public Player(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
