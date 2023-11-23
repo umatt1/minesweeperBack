c53f6a2ef89016fcfb959365dae50a700646b59a
umatt1 <matt.burkhard@gmail.com>
2023-11-22 18:37:40 -0600

    Implements puzzle controller

--- minesweeper/src/main/java/com/minesweeper/controller/PuzzleController.java
+++ minesweeper/src/main/java/com/minesweeper/controller/PuzzleController.java
@@ -1,9 +1,12 @@
 package com.minesweeper.controller;
 
+import com.minesweeper.model.Puzzle;
 import com.minesweeper.service.PuzzleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 // PuzzleController.java
 @RestController
@@ -13,4 +16,24 @@ public class PuzzleController {
     private PuzzleService puzzleService;
 
     // CRUD operations for Puzzle entity
+
+    @GetMapping
+    public List<Puzzle> getAllPuzzles() {
+        return puzzleService.getAllPuzzles();
+    }
+
+    @GetMapping("/{puzzleId}")
+    public Optional<Puzzle> getPuzzleById(@PathVariable Long puzzleId) {
+        return puzzleService.getPuzzleById(puzzleId);
+    }
+
+    @PostMapping
+    public Puzzle createPuzzle(@RequestBody Puzzle puzzle) {
+        return puzzleService.createPuzzle(puzzle);
+    }
+
+    @DeleteMapping("/{puzzleId}")
+    public void deletePlayer(@PathVariable Long puzzleId) {
+        puzzleService.deletePuzzle(puzzleId);
+    }
 }
