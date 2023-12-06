25e85266896e2bff304a816764531203d7e0a77a
umatt1 <matt.burkhard@gmail.com>
2023-12-06 00:46:49 -0600

    Start to change

--- src/main/java/com/minesweeper/service/PuzzleService.java
+++ src/main/java/com/minesweeper/service/PuzzleService.java
@@ -2,6 +2,8 @@ package com.minesweeper.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 import com.minesweeper.repo.PuzzleRepository;
@@ -34,4 +36,23 @@ public class PuzzleService {
     }
 
     // You can add additional methods as needed
+
+    public Puzzle createPuzzleOfTheDay() {
+        Puzzle puzzle = new Puzzle();
+
+        // Set the date to the current date
+        puzzle.setDate(new Date());
+
+        // Your logic to generate the layout for the puzzle
+        // For example, you can call a method to generate a random layout
+        List<List<Integer>> layout = generateRandomLayout();
+        puzzle.setLayout(layout);
+
+        // Save the puzzle to the database
+        return puzzleRepository.save(puzzle);
+    }
+
+    public Optional<Puzzle> getPuzzleByDate(LocalDate date) {
+        return puzzleRepository.findByDate(date);
+    }
 }
