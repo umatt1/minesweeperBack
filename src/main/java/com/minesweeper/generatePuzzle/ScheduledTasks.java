a769a540364f1029422a0c8691545b1d89aab933
umatt1 <matt.burkhard@gmail.com>
2023-12-07 23:37:02 -0600

    Create cronjob

---
+++ src/main/java/com/minesweeper/generatePuzzle/ScheduledTasks.java
@@ -0,0 +1,19 @@
+package com.minesweeper.generatePuzzle;
+
+import com.minesweeper.controller.PuzzleController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    @Autowired
+    PuzzleController puzzleController;
+
+    @Scheduled(cron = "0 0 0 * * ?") // Run at midnight every night
+    public void createPuzzle() {
+        System.out.println("Creating puzzle of the day");
+        puzzleController.createPuzzleOfTheDay();
+    }
+}
