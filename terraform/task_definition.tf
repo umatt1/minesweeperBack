cb20422f782a99ffb40b54ccaf1dc1dd168b071b
umatt1 <matt.burkhard@gmail.com>
2024-04-17 22:49:40 -0400

    Change docker-compose, terraform

--- terraform/task_definition.tf
+++ terraform/task_definition.tf
@@ -1,4 +1,5 @@
 resource "aws_ecs_task_definition" "task_definition" {
   family = "worker"
-  container_definitions = data.template_file.task-definition_template.rendered
+  #container_definitions = data.template_file.task-definition_template.rendered
+  container_definitions = local.task_definition_template
 }
--- terraform/template_file.tf
+++ terraform/template_file.tf
@@ -1,8 +1,8 @@
 # https://stackoverflow.com/questions/74508452/migrate-from-template-file-to-templatefile
 
-data "template_file" "task-definition_template" {
-    template = file("task_definition.json.tpl")
-    vars = {
+locals {
+    task_definition_template = templatefile("task_definition.json.tpl", {
         REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
-    }
+    })
 }
+
