9ad5372d5dfb584af5ab7ead073ff423e8a718a0
umatt1 <matt.burkhard@gmail.com>
2024-07-03 23:32:25 -0500

    Fix attempt more

--- src/main/java/com/minesweeper/config/SecurityConfig.java
+++ src/main/java/com/minesweeper/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(allowedOrigin, "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
